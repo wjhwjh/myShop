73c1f2982e64294e8be737abf98b34985038042e
wjhwjh <1468017149@qq.com>
2020-05-18 15:12:11 +0800

    tijiao

--- src/components/photo/data.js
+++ src/components/photo/data.js
@@ -137,12 +137,12 @@ const dataPhotoDetail = [{
 const dataPhotoDetailImg = [{
         id: '1',
         imgUrlBig: [
-            '../../../static/img/photo1.jpg',
-            '../../../static/img/photo2.jpg',
-            '../../../static/img/photo3.jpg',
-            '../../../static/img/photo4.jpg',
-            '../../../static/img/photo5.jpg',
-            '../../../static/img/photo6.jpg'
+            'https://img3.doubanio.com/view/photo/m/public/p2576090251.webp',
+            'https://img9.doubanio.com/view/photo/m/public/p2575864385.webp',
+            'https://img3.doubanio.com/view/photo/m/public/p2577442100.webp',
+            'https://img1.doubanio.com/view/photo/m/public/p2574259049.webp',
+            'https://img1.doubanio.com/view/photo/m/public/p2578619867.webp',
+            'https://img3.doubanio.com/view/photo/m/public/p2575373020.webp'
         ],
     },
     {
@@ -181,8 +181,54 @@ const dataPhotoDetailImg = [{
             'https://img3.doubanio.com/view/photo/m/public/p2575373020.webp'
         ],
     },
+    {
 
+        id: '5',
+        imgUrlBig: [
+            'https://img3.doubanio.com/view/photo/m/public/p2576090251.webp',
+            'https://img9.doubanio.com/view/photo/m/public/p2575864385.webp',
+            'https://img3.doubanio.com/view/photo/m/public/p2577442100.webp',
+            'https://img1.doubanio.com/view/photo/m/public/p2574259049.webp',
+            'https://img1.doubanio.com/view/photo/m/public/p2578619867.webp',
+            'https://img3.doubanio.com/view/photo/m/public/p2575373020.webp'
+        ],
+    },
+    {
 
+        id: '6',
+        imgUrlBig: [
+            'https://img3.doubanio.com/view/photo/m/public/p2576090251.webp',
+            'https://img9.doubanio.com/view/photo/m/public/p2575864385.webp',
+            'https://img3.doubanio.com/view/photo/m/public/p2577442100.webp',
+            'https://img1.doubanio.com/view/photo/m/public/p2574259049.webp',
+            'https://img1.doubanio.com/view/photo/m/public/p2578619867.webp',
+            'https://img3.doubanio.com/view/photo/m/public/p2575373020.webp'
+        ],
+    },
+    {
+
+        id: '7',
+        imgUrlBig: [
+            'https://img3.doubanio.com/view/photo/m/public/p2576090251.webp',
+            'https://img9.doubanio.com/view/photo/m/public/p2575864385.webp',
+            'https://img3.doubanio.com/view/photo/m/public/p2577442100.webp',
+            'https://img1.doubanio.com/view/photo/m/public/p2574259049.webp',
+            'https://img1.doubanio.com/view/photo/m/public/p2578619867.webp',
+            'https://img3.doubanio.com/view/photo/m/public/p2575373020.webp'
+        ],
+    },
+    {
+
+        id: '8',
+        imgUrlBig: [
+            'https://img3.doubanio.com/view/photo/m/public/p2576090251.webp',
+            'https://img9.doubanio.com/view/photo/m/public/p2575864385.webp',
+            'https://img3.doubanio.com/view/photo/m/public/p2577442100.webp',
+            'https://img1.doubanio.com/view/photo/m/public/p2574259049.webp',
+            'https://img1.doubanio.com/view/photo/m/public/p2578619867.webp',
+            'https://img3.doubanio.com/view/photo/m/public/p2575373020.webp'
+        ],
+    },
 ]
 
 
--- src/components/photo/photoDetail.vue
+++ src/components/photo/photoDetail.vue
@@ -2,48 +2,86 @@
   <div>
     <navBar title="图片详情"></navBar>
     <div class="content">
-        <!-- {{dataPhotoImg}}
-        {{dataPhotoImg[0]}}
-        <img :src="dataPhotoImg[0]" alt="" width="100%" height="200px"> -->
-        <ul>
-          <li v-for="{url, index} in dataPhotoImg.imgUrlBig" :key="index">
-             <!-- <img :src="url" alt="" width="100%" height="200px"> -->
-             {{url}}
-             {{idex}}
-          </li>
-        </ul>
+      <div class="tit-wrap">
+        <h2>{{dataPhotoCurrent.title}}</h2>
+        <p>
+          <span>国籍：{{dataPhotoCurrent.from}}</span>
+          <span>时长：{{dataPhotoCurrent.timeLength}}</span>
+        </p>
+      </div>
+      <p class="des">{{dataPhotoCurrent.content}}</p>
+      <ul class="img-wrap">
+        <li v-for="(item, idx) in dataPhotoArray" :key="idx">
+          <!-- img 一定要闭合标签，不然渲染就不会成功 -->
+          <img :src="item" alt width="100%" height="auto" />
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 <script>
-import { dataPhotoDetailImg } from "./data.js";
+import { dataPhotoList, dataPhotoDetailImg } from "./data.js";
+
 export default {
   data() {
     return {
-      dataPhotoImg:[],
+      dataPhotoArray: [],
+      dataPhotoCurrent: {}
     };
   },
-  mounted() {
-    //console.log(dataPhotoDetailImg);
-
-       console.log(this.$route.query.id);
-      // 这里图片的借口和文案内容的接口是不同的，在请求后台的时候需要进行并发请求
-      // 图片预览功能
-      let queryId = this.$route.query.id;
-      this.dataPhotoImg = dataPhotoDetailImg.filter(item=>item.id==queryId)[0]
-
-     console.log(typeof this.dataPhotoImg);
-  },
+  mounted() {},
   created() {
-   
-      
-
-
-
-
+    // 这里图片的借口和文案内容的接口是不同的，在请求后台的时候需要进行并发请求
+    // 图片预览功能
+    let queryId = this.$route.query.id;
+    let dataPhotoImg = {};
+    dataPhotoImg = dataPhotoDetailImg.filter(item => item.id == queryId)[0];
+    this.dataPhotoArray = dataPhotoImg["imgUrlBig"];
+    //console.log("判断是否是数组", this.dataPhotoArray instanceof Array);
+    //当前列表数据
+    this.dataPhotoCurrent = dataPhotoList.filter(item => item.id == queryId)[0];
+    console.log("当前数据", this.dataPhotoCurrent);
   },
   methods: {}
 };
 </script>
 <style lang="css" scoped>
+.content {
+  padding: 15px 15px;
+}
+.img-wrap {
+  display: flex;
+  flex-wrap: wrap;
+}
+.img-wrap li {
+  flex: 1 1 33.33%;
+  padding: 5px;
+}
+.img-wrap li img {
+  width: 100%;
+  display: block;
+}
+
+.tit-wrap {
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 10px;
+}
+.tit-wrap h2 {
+  font-size: 16px;
+  color: #222;
+  padding-bottom: 10px;
+}
+.tit-wrap p {
+  font-size: 14px;
+}
+.tit-wrap p span {
+  margin-right: 20px;
+}
+.des {
+  line-height: 1.8;
+  font-size: 14px;
+  color: #666;
+  padding-top: 15px;
+  padding-bottom: 10px;
+}
 </style>
