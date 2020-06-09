29823e7963b8793e3bb01f643c5029047dc34254
wjhwjh <1468017149@qq.com>
2020-06-09 13:50:25 +0800

    提交

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,6 +2,24 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
+
+/*引入自己的数据*/
+// webpack是基于node.js，所以在使用的时候结合node
+// webpack 也是一个包，需要进行配置
+// 如何玩转webpack,如何配置，都有那些作用
+const express = require('express')
+const app = express()
+let appData = require('../data.json')
+
+console.log('数据---', appData);
+
+let seller = appData.seller
+let goods = appData.goods
+let ratings = appData.ratings
+
+let apiRouter = express.Router()
+app.use('/api', apiRouter)
+
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+    NODE_ENV: '"development"'
 })
